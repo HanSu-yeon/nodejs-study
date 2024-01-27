6f544e3ff838714f7c2ce51381599f848e2ff010
HanSuYeon <js7122420@gmail.com>
2024-01-27 23:43:25 +0900

    파일 이동

--- ch02/code3-1-ok-server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-//createServer로 서버 인스턴스 생성
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html'); //1. 응답의 헤더 설정
-  res.end('OK'); //2. 'ok'를 응답하고 종료
-});
-//list(port, 서버가 시작될 때 실행하는 콜백 함수)
-server.listen('3000', () => console.log('OK 서버 시작')); //3. 접속 대기
